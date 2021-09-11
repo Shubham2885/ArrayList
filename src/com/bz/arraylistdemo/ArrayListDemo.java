2b117f2c9219282834008d55a2c2779b984faffd
Shubham2885 <shubham.chavan689977@gmail.com>
2021-09-11 18:25:18 +0530

    first commit

---
+++ src/com/bz/arraylistdemo/ArrayListDemo.java
@@ -0,0 +1,34 @@
+package com.bz.arraylistdemo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> arrayList = new ArrayList<String>();
+		
+		arrayList.add("1");
+		arrayList.add("2");
+		System.out.println(arrayList.add("3"));
+		arrayList.add("4");
+		arrayList.add("5");
+		
+				
+		for(String string : arrayList) {
+			System.out.println("List of content :: "+string);
+		}
+		
+		arrayList.forEach(string ->{
+			System.out.println("Content :: "+string);
+		});
+		
+		System.out.println(arrayList.contains("7"));
+		
+		System.out.println(arrayList.get(2));
+		
+		
+	}
+}
---
+++ src/com/bz/arraylistdemo/HashMapDemo.java
@@ -0,0 +1,16 @@
+package com.bz.arraylistdemo;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
+		
+		hashMap.put(1, "Shubham");
+		hashMap.put(2, "Shubham");
+		
+		System.out.println(hashMap);
+	}
+}
